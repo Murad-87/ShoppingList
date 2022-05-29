ffae0d746e9bbd590bb5c2214f37101712badc03
Murad <Murad-87>
2022-05-29 21:21:06 +0300

    Добавлена работа с фрагментом и реализована альбомная ориентация экрана

---
+++ app/src/main/java/com/example/myshoppinglist/utils/util.kt
@@ -0,0 +1,19 @@
+package com.example.myshoppinglist.utils
+
+import android.content.Context
+import android.content.Intent
+import com.example.myshoppinglist.presentation.ShopItemActivity
+import com.example.myshoppinglist.presentation.ShopItemFragment
+
+//fun newIntentAddItem(context: Context): Intent {
+//    val intent = Intent(context, ShopItemActivity::class.java)
+//    intent.putExtra(ShopItemFragment.SCREEN_MODE, ShopItemFragment.MODE_ADD)
+//    return intent
+//}
+//
+//fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+//    val intent = Intent(context, ShopItemActivity::class.java)
+//    intent.putExtra(ShopItemFragment.SCREEN_MODE, ShopItemFragment.MODE_EDIT)
+//    intent.putExtra(ShopItemFragment.SHOP_ITEM_ID, shopItemId)
+//    return intent
+//}
